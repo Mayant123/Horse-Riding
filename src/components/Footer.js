5a00605d51fc8afc468c519fb594f2419d779e42
maynat123 <maynatrajput511999@gmail.com>
2024-08-27 10:46:09 -0700

    Upadting code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import logo from "../assets/img/header_logo.jpg";
 const Footer = () => {
   return (
     <footer className="relative overflow-hidden bg-[#00c2e9] text-white font-serif py-12">
-      {/* Background Image */}
+
       <div
         className="absolute inset-0 z-0"
         style={{
@@ -16,11 +16,8 @@ const Footer = () => {
           backgroundSize: 'cover',
         }}
       ></div>
-
-      {/* Foreground Content */}
       <div className="relative z-10 container mx-auto lg:px-[115px] px-4">
         <div className="flex flex-col lg:flex-row justify-between items-start">
-          {/* Left Section - Logo and Description */}
           <div className="flex flex-col mb-6 lg:mb-0 lg:w-[30%]">
             <div className='w-auto h-auto rounded-md'>
               <img src={logo} alt="Logo" className="mb-4 max-w-[150px] h-auto rounded-md" />
@@ -31,8 +28,6 @@ const Footer = () => {
             <p className="text-gray-200">Mumbai / Pune, Bengaluru</p>
             <p className="text-gray-200">&copy; {new Date().getFullYear()} THK INTERNATIONAL TRADING. All rights reserved.</p>
           </div>
-
-          {/* Middle Section - Links */}
           <div className="flex flex-col mb-6 lg:mb-0 lg:w-[20%]">
             <h3 className="text-xl font-semibold mb-4">Quick Links</h3>
             <ul className="space-y-2">
@@ -42,8 +37,6 @@ const Footer = () => {
               <li><Link to="#" className="hover:text-gray-100">Contact</Link></li>
             </ul>
           </div>
-
-          {/* Right Section - Contact Info and Social Icons */}
           <div className="flex flex-col lg:w-[30%]">
             <h3 className="text-xl font-semibold mb-4">Contact Us</h3>
             <p className="text-gray-200">P.O. Box 1519, Post code 122, AL Mabela Muscat, Sultanate of Oman</p>
@@ -66,8 +59,6 @@ const Footer = () => {
             </div>
           </div>
         </div>
-
-        {/* Footer Bottom Links */}
         <div className="mt-12 flex justify-between text-gray-200 text-sm">
           <div>
             <Link to="#" className="hover:underline">Terms and Conditions</Link>
